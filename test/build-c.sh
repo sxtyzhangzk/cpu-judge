46cf14cc9e4420fa5bcd4f8107191debb9f95e21
张哲恺 <sxtyzhangzk@live.cn>
2018-01-06 12:00:50 +0800

    Add simulation & link newlib when building testcase

--- test/build-c.sh
+++ test/build-c.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
 rpath="/opt/riscv/bin/"
-${rpath}riscv32-unknown-elf-gcc -o ram.o -c $1 -O2 -march=rv32i -mabi=ilp32
+${rpath}riscv32-unknown-elf-gcc -o ram.o -c $1 -O2 -march=rv32i -mabi=ilp32 -Wall
+${rpath}riscv32-unknown-elf-gcc -o system.o -c system.c -O2 -march=rv32i -mabi=ilp32 -Wall
 ${rpath}riscv32-unknown-elf-as -o rom.o -march=rv32i rom.s 
-${rpath}riscv32-unknown-elf-ld -T memory.ld rom.o ram.o -o memory.om
+${rpath}riscv32-unknown-elf-ld -T memory.ld rom.o ram.o system.o -L $rpath/../riscv32-unknown-elf/lib/ -L $rpath/../lib/gcc/riscv32-unknown-elf/7.2.0/ -lc -lgcc -lm -lnosys -o memory.om
 ${rpath}riscv32-unknown-elf-objcopy -O binary memory.om memory.bin
